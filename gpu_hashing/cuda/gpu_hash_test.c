8932597d2e163ec365aa6dc681634841d2545d83
moumou <moumou3@users.noreply.github.com>
2019-02-13 00:51:54 +0900

    change of gettimeofday to rdtsc in cuda

--- gpu_hashing/cuda/gpu_hash_test.c
+++ gpu_hashing/cuda/gpu_hash_test.c
@@ -5,7 +5,6 @@
 #define printf printk
 #define malloc vmalloc
 #define free vfree
-#define gettimeofday(x, y) do_gettimeofday(x)
 #else /* just for measurement */
 #include <sys/time.h>
 #include <stdio.h>
@@ -15,16 +14,13 @@
 
 #define PAGESIZE 4096
 /* tvsub: ret = x - y. */
-static inline void tvsub(struct timeval *x, 
-						 struct timeval *y, 
-						 struct timeval *ret)
-{
-	ret->tv_sec = x->tv_sec - y->tv_sec;
-	ret->tv_usec = x->tv_usec - y->tv_usec;
-	if (ret->tv_usec < 0) {
-		ret->tv_sec--;
-		ret->tv_usec += 1000000;
-	}
+
+static inline unsigned long long rdtsc() {
+  unsigned long long ret;
+
+  __asm__ volatile ("rdtsc" : "=A" (ret));
+
+  return ret;
 }
 
 int cuda_test_hash(unsigned int n, char *path)
@@ -42,25 +38,24 @@ int cuda_test_hash(unsigned int n, char *path)
 	unsigned int *c = (unsigned int *) malloc (n*n * sizeof(unsigned int));
 	int block_x, block_y, grid_x, grid_y;
 	char fname[256];
-	struct timeval tv;
-	struct timeval tv_total_start, tv_total_end;
-	float total;
-	struct timeval tv_h2d_start, tv_h2d_end;
-	float h2d;
-	struct timeval tv_d2h_start, tv_d2h_end;
-	float d2h;
-	struct timeval tv_exec_start, tv_exec_end;
-	struct timeval tv_mem_alloc_start;
-	struct timeval tv_data_init_start;
-	float data_init;
-	struct timeval tv_conf_kern_start;
-	struct timeval tv_close_start;
-	float mem_alloc;
-	float exec;
-	float init_gpu;
-	float configure_kernel;
-	float close_gpu;
-	float data_read;
+	unsigned long long tv_total_start, tv_total_end;
+	unsigned long long total;
+	unsigned long long tv_h2d_start, tv_h2d_end;
+	unsigned long long h2d;
+	unsigned long long tv_d2h_start, tv_d2h_end;
+	unsigned long long d2h;
+	unsigned long long tv_exec_start, tv_exec_end;
+	unsigned long long tv_mem_alloc_start;
+	unsigned long long tv_data_init_start;
+	unsigned long long data_init;
+	unsigned long long tv_conf_kern_start;
+	unsigned long long tv_close_start;
+	unsigned long long mem_alloc;
+	unsigned long long exec;
+	unsigned long long init_gpu;
+	unsigned long long configure_kernel;
+	unsigned long long close_gpu;
+	unsigned long long data_read;
 
 	unsigned int dummy_b, dummy_c;
 	
@@ -77,7 +72,7 @@ int cuda_test_hash(unsigned int n, char *path)
 	printf("block = (%d, %d)\n", block_x, block_y);
 	printf("grid = (%d, %d)\n", grid_x, grid_y);
 
-	gettimeofday(&tv_total_start, NULL);
+	tv_total_start = rdtsc();
 
 	res = cuInit(0);
 	if (res != CUDA_SUCCESS) {
@@ -114,7 +109,7 @@ int cuda_test_hash(unsigned int n, char *path)
 		return -1;
 	}
 
-	gettimeofday(&tv_mem_alloc_start, NULL);
+	tv_mem_alloc_start = rdtsc();
 
 
 	/* a[] */
@@ -129,26 +124,25 @@ int cuda_test_hash(unsigned int n, char *path)
 		printf("cuMemAlloc (c) failed\n");
 		return -1;
 	}
-	gettimeofday(&tv_data_init_start, NULL);
+	tv_data_init_start = rdtsc();
 
 	/* initialize A[] & B[] */
         memset(a, 0x5, PAGESIZE);
-        
 
 
-	gettimeofday(&tv_h2d_start, NULL);
+	tv_h2d_start = rdtsc();
 	/* upload a[] and b[] */
 	res = cuMemcpyHtoD(text1_dev, a, PAGESIZE);
 	if (res != CUDA_SUCCESS) {
 		printf("cuMemcpyHtoD (a) failed: res = %lu\n", (unsigned long)res);
 		return -1;
 	}
-	gettimeofday(&tv_h2d_end, NULL);
+	tv_h2d_end = rdtsc();
 
-	gettimeofday(&tv_conf_kern_start, NULL);
+	tv_conf_kern_start = rdtsc();
 
 	/* set kernel parameters */
-	res = cuParamSeti(function, 0, text1_dev);	
+	res = cuParamSeti(function, 0, text1_dev);
 	if (res != CUDA_SUCCESS) {
 		printf("cuParamSeti (a) failed: res = %lu\n", (unsigned long)res);
 		return -1;
@@ -179,7 +173,7 @@ int cuda_test_hash(unsigned int n, char *path)
 		return -1;
 	}
 
-	gettimeofday(&tv_exec_start, NULL);
+	tv_exec_start = rdtsc();
 	/* launch the kernel */
 	res = cuLaunchGrid(function, grid_x, grid_y);
 	if (res != CUDA_SUCCESS) {
@@ -187,9 +181,9 @@ int cuda_test_hash(unsigned int n, char *path)
 		return -1;
 	}
 	cuCtxSynchronize();
-	gettimeofday(&tv_exec_end, NULL);
+	tv_exec_end = rdtsc();
 
-	gettimeofday(&tv_d2h_start, NULL);
+	tv_d2h_start = rdtsc();
 	/* download c[] */
 	res = cuMemcpyDtoH(&pass, pass_dev, sizeof(unsigned char));
         printf("pass hash value 0x%x \n", pass);
@@ -197,10 +191,10 @@ int cuda_test_hash(unsigned int n, char *path)
 		printf("cuMemcpyDtoH (c) failed: res = %lu\n", (unsigned long)res);
 		return -1;
 	}
-	gettimeofday(&tv_d2h_end, NULL);
+	tv_d2h_end = rdtsc();
 
 
-	gettimeofday(&tv_close_start, NULL);
+	tv_close_start = rdtsc();
 
 	res = cuMemFree(text1_dev);
 	if (res != CUDA_SUCCESS) {
@@ -229,49 +223,30 @@ int cuda_test_hash(unsigned int n, char *path)
 	free(b);
 	free(c);
 
-	gettimeofday(&tv_total_end, NULL);
+	tv_total_end = rdtsc();
 
 
-	tvsub(&tv_mem_alloc_start, &tv_total_start, &tv);
-	init_gpu = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
+	init_gpu = tv_mem_alloc_start - tv_total_start;
+	mem_alloc = tv_data_init_start - tv_mem_alloc_start;
+	data_init = tv_h2d_start - tv_data_init_start;
+	h2d = tv_h2d_end - tv_h2d_start;
+	configure_kernel = tv_exec_start - tv_conf_kern_start;
+	exec = tv_exec_end - tv_exec_start;
+	d2h = tv_d2h_end - tv_d2h_start;
+	data_read = tv_close_start - tv_d2h_end;
+	close_gpu = tv_total_end - tv_close_start;
+	total = tv_total_end - tv_total_start;
 
-	tvsub(&tv_data_init_start, &tv_mem_alloc_start, &tv);
-	mem_alloc = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
-
-	tvsub(&tv_h2d_start, &tv_data_init_start, &tv);
-	data_init = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
-
-	tvsub(&tv_h2d_end, &tv_h2d_start, &tv);
-	h2d = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
-
-	tvsub(&tv_exec_start, &tv_conf_kern_start, &tv);
-	configure_kernel = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
-
-	tvsub(&tv_exec_end, &tv_exec_start, &tv);
-	exec = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
-
-	tvsub(&tv_d2h_end, &tv_d2h_start, &tv);
-	d2h = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
-
-	tvsub(&tv_close_start, &tv_d2h_end, &tv);
-	data_read = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
-
-	tvsub(&tv_total_end, &tv_close_start, &tv);
-	close_gpu = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
-
-	tvsub(&tv_total_end, &tv_total_start, &tv);
-	total = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
-
-	printf("Init: %f\n", init_gpu);
-	printf("MemAlloc: %f\n", mem_alloc);
-	printf("DataInit: %f\n", data_init);
-	printf("HtoD: %f\n", h2d);
-	printf("KernConf: %f\n", configure_kernel);
-	printf("Exec: %f\n", exec);
-	printf("DtoH: %f\n", d2h);
-	printf("DataRead: %f\n", data_read);
-	printf("Close: %f\n", close_gpu);
-	printf("Total: %f\n", total);
+	printf("Init: %llu\n", init_gpu);
+	printf("MemAlloc: %llu\n", mem_alloc);
+	printf("DataInit: %llu\n", data_init);
+	printf("HtoD: %llu\n", h2d);
+	printf("KernConf: %llu\n", configure_kernel);
+	printf("Exec: %llu\n", exec);
+	printf("DtoH: %llu\n", d2h);
+	printf("DataRead: %llu\n", data_read);
+	printf("Close: %llu\n", close_gpu);
+	printf("Total: %llu\n", total);
 
 	return 0;
 }

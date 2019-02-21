53e0ef1f3ddf6e9276db1aa0b31e3939bd95dabb
moumou <moumou3@users.noreply.github.com>
2019-02-21 15:53:08 +0900

    debug

--- ksmtest/create_testfile.c
+++ ksmtest/create_testfile.c
@@ -5,7 +5,7 @@
 
 /*
 ---file example----
-50% shareing_potential & distance 1
+50% sharing_potential & distance 1
 ffffffff0000... 000000010000...
 
 */
@@ -32,19 +32,21 @@ int main(int argc, char *argv[])
   uint32_t uint32_index = 0;
   uint32_t uint32_max = ~uint32_index;
   char buffer[PAGESIZE*10 + 1] = {0};
+  int sh = 0;
+  int sharable_pagenum = sharing_potential * filesize / 100 / PAGESIZE;
   
   if ((distance + 1) * sharing_potential > 100)
     printf("distance or sharing_potential is too learge\n");
 
   for (i = 0; i < filesize / PAGESIZE; ++i) {
-    if (d == 0) 
+    if (d == 0 && sh++ < sharable_pagenum) 
       createpage(uint32_max, fp);
     else 
       createpage(uint32_index++, fp);
 
     d = (d == distance) ? 0:(d+1);
   }
-  FILE *fp = fopen("testfile.txt", "r");
+  fp = fopen("testfile.txt", "r");
   fread(buffer, PAGESIZE*10, 1, fp);
   printf("buffer:%s", buffer);
   

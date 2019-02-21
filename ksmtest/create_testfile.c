b673eb12a9f8059b99a117de3a1e90bd36d2d815
moumou <moumou3@users.noreply.github.com>
2019-02-21 15:00:52 +0900

    add create_testfile.c for creating file of specific sharing potential

---
+++ ksmtest/create_testfile.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+---file example----
+50% shareing_potential & distance 1
+ffffffff0000... 000000010000...
+
+*/
+
+#define PAGESIZE __SC_PAGESIZE
+
+void createpage(uint32_t uint32_index, FILE *fp) {
+  int i;
+  
+  fwrite(&uint32_index, sizeof(uint32_t), 1, fp);
+  for (i = 0; i < PAGESIZE - sizeof(uint32_t); ++i) {
+    putc(0x0, fp);
+  }
+
+}
+int main(int argc, char *argv[])
+{
+  FILE *fp = fopen("testfile.txt", "w");
+  size_t filesize = atoi(argv[1]);
+  float shareing_potential = atoi(argv[2]) / 100;
+  int distance = 0;
+  int d = 0;
+  uint32_t uint32_index = 0;
+  uint32_t uint32_max = ~uint32_index;
+  
+  if ((distance + 1) * shareing_potential > 1)
+    printf("distance or sharing_potential is too learge\n");
+
+  for (i = 0; i < filesize / PAGESIZE; ++i) {
+    if (d == 0) 
+      createpage(uint32_max, fp);
+    else 
+      createpage(uint32_index++, fp);
+
+    d = (d == distance) ? 0:(d+1);
+  }
+  
+  return 0;
+}

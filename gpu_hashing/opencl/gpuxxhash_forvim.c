d884c34bb6f388c738ad7e26dc6b25390dd6602d
moumou <moumou3@users.noreply.github.com>
2019-05-19 20:27:23 +0900

    Modify gpuxxhash_forvim.c for xxhash

--- gpu_hashing/opencl/gpuxxhash_forvim.c
+++ gpu_hashing/opencl/gpuxxhash_forvim.c
@@ -1,173 +1,95 @@
-typedef unsigned char BYTE;             // 8-bit byte
-typedef unsigned int  WORD;             // 32-bit word, change to "long" for 16-bit machines
+typedef unsigned char uint8_t;             // 8-bit byte
+typedef unsigned int  uint32_t;             // 32-bit word, change to "long" for 16-bit machines
 
-typedef struct {
-	BYTE data[64];
-	WORD datalen;
-	unsigned long long bitlen;
-	WORD state[5];
-	WORD k[4];
-} SHA1_CTX;
 
-#define ROTLEFT(a, b) ((a << b) | (a >> (32 - b)))
-#define SHA1_BLOCK_SIZE 20              // SHA1 outputs a 20 byte digest
 #define PAGE_SIZE 4096
+#define xxh_rotl32(x, r) ((x << r) | (x >> (32 - r)))
 
-void sha1_init(SHA1_CTX *ctx)
+__constant uint32_t PRIME32_1 = 2654435761U;
+__constant uint32_t PRIME32_2 = 2246822519U;
+__constant int32_t PRIME32_3 = 3266489917U;
+__constant uint32_t PRIME32_4 =  668265263U;
+__constant uint32_t PRIME32_5 =  374761393U;
+
+uint32_t get_unaligned_le32(const uint8_t *p)
 {
-	ctx->datalen = 0;
-	ctx->bitlen = 0;
-	ctx->state[0] = 0x67452301;
-	ctx->state[1] = 0xEFCDAB89;
-	ctx->state[2] = 0x98BADCFE;
-	ctx->state[3] = 0x10325476;
-	ctx->state[4] = 0xc3d2e1f0;
-	ctx->k[0] = 0x5a827999;
-	ctx->k[1] = 0x6ed9eba1;
-	ctx->k[2] = 0x8f1bbcdc;
-	ctx->k[3] = 0xca62c1d6;
+	return p[0] | p[1] << 8 | p[2] << 16 | p[3] << 24;
 }
 
-void sha1_transform(SHA1_CTX *ctx, const BYTE data[])
+uint32_t xxh32_round(uint32_t seed, const uint32_t input)
 {
-	WORD a, b, c, d, e, i, j, t, m[80];
-
-	for (i = 0, j = 0; i < 16; ++i, j += 4)
-		m[i] = (data[j] << 24) + (data[j + 1] << 16) + (data[j + 2] << 8) + (data[j + 3]);
-	for ( ; i < 80; ++i) {
-		m[i] = (m[i - 3] ^ m[i - 8] ^ m[i - 14] ^ m[i - 16]);
-		m[i] = (m[i] << 1) | (m[i] >> 31);
-	}
-
-	a = ctx->state[0];
-	b = ctx->state[1];
-	c = ctx->state[2];
-	d = ctx->state[3];
-	e = ctx->state[4];
-
-	for (i = 0; i < 20; ++i) {
-		t = ROTLEFT(a, 5) + ((b & c) ^ (~b & d)) + e + ctx->k[0] + m[i];
-		e = d;
-		d = c;
-		c = ROTLEFT(b, 30);
-		b = a;
-		a = t;
-	}
-	for ( ; i < 40; ++i) {
-		t = ROTLEFT(a, 5) + (b ^ c ^ d) + e + ctx->k[1] + m[i];
-		e = d;
-		d = c;
-		c = ROTLEFT(b, 30);
-		b = a;
-		a = t;
-	}
-	for ( ; i < 60; ++i) {
-		t = ROTLEFT(a, 5) + ((b & c) ^ (b & d) ^ (c & d))  + e + ctx->k[2] + m[i];
-		e = d;
-		d = c;
-		c = ROTLEFT(b, 30);
-		b = a;
-		a = t;
-	}
-	for ( ; i < 80; ++i) {
-		t = ROTLEFT(a, 5) + (b ^ c ^ d) + e + ctx->k[3] + m[i];
-		e = d;
-		d = c;
-		c = ROTLEFT(b, 30);
-		b = a;
-		a = t;
-	}
-
-	ctx->state[0] += a;
-	ctx->state[1] += b;
-	ctx->state[2] += c;
-	ctx->state[3] += d;
-	ctx->state[4] += e;
+	seed += input * PRIME32_2;
+	seed = xxh_rotl32(seed, 13);
+	seed *= PRIME32_1;
+	return seed;
 }
 
-void sha1_update(SHA1_CTX *ctx, const BYTE data[], size_t len)
+uint32_t xxh32(const void *input, const size_t len, const uint32_t seed)
 {
-	size_t i;
+	const uint8_t *p = (const uint8_t *)input;
+	const uint8_t *b_end = p + len;
+	uint32_t h32;
 
-	for (i = 0; i < len; ++i) {
-		ctx->data[ctx->datalen] = data[i];
-		ctx->datalen++;
-		if (ctx->datalen == 64) {
-			sha1_transform(ctx, ctx->data);
-			ctx->bitlen += 512;
-			ctx->datalen = 0;
-		}
+	if (len >= 16) {
+		const uint8_t *const limit = b_end - 16;
+		uint32_t v1 = seed + PRIME32_1 + PRIME32_2;
+		uint32_t v2 = seed + PRIME32_2;
+		uint32_t v3 = seed + 0;
+		uint32_t v4 = seed - PRIME32_1;
+
+		do {
+			v1 = xxh32_round(v1, get_unaligned_le32(p));
+			p += 4;
+			v2 = xxh32_round(v2, get_unaligned_le32(p));
+			p += 4;
+			v3 = xxh32_round(v3, get_unaligned_le32(p));
+			p += 4;
+			v4 = xxh32_round(v4, get_unaligned_le32(p));
+			p += 4;
+		} while (p <= limit);
+
+		h32 = xxh_rotl32(v1, 1) + xxh_rotl32(v2, 7) +
+			xxh_rotl32(v3, 12) + xxh_rotl32(v4, 18);
+	} else {
+		h32 = seed + PRIME32_5;
 	}
+
+	h32 += (uint32_t)len;
+
+	while (p + 4 <= b_end) {
+		h32 += get_unaligned_le32(p) * PRIME32_3;
+		h32 = xxh_rotl32(h32, 17) * PRIME32_4;
+		p += 4;
+	}
+
+	while (p < b_end) {
+		h32 += (*p) * PRIME32_5;
+		h32 = xxh_rotl32(h32, 11) * PRIME32_1;
+		p++;
+	}
+
+	h32 ^= h32 >> 15;
+	h32 *= PRIME32_2;
+	h32 ^= h32 >> 13;
+	h32 *= PRIME32_3;
+	h32 ^= h32 >> 16;
+
+	return h32;
 }
 
-void sha1_final(SHA1_CTX *ctx, BYTE hash[])
-{
-	WORD i;
-
-	i = ctx->datalen;
-
-	// Pad whatever data is left in the buffer.
-	if (ctx->datalen < 56) {
-		ctx->data[i++] = 0x80;
-		while (i < 56)
-			ctx->data[i++] = 0x00;
-	}
-	else {
-		ctx->data[i++] = 0x80;
-		while (i < 64)
-			ctx->data[i++] = 0x00;
-		sha1_transform(ctx, ctx->data);
-		while (i < 7)
-		  ctx->data[i++] = 0x00;
-	}
-
-	// Append to the padding the total message's length in bits and transform.
-	ctx->bitlen += ctx->datalen * 8;
-	ctx->data[63] = ctx->bitlen;
-	ctx->data[62] = ctx->bitlen >> 8;
-	ctx->data[61] = ctx->bitlen >> 16;
-	ctx->data[60] = ctx->bitlen >> 24;
-	ctx->data[59] = ctx->bitlen >> 32;
-	ctx->data[58] = ctx->bitlen >> 40;
-	ctx->data[57] = ctx->bitlen >> 48;
-	ctx->data[56] = ctx->bitlen >> 56;
-	sha1_transform(ctx, ctx->data);
-
-	// Since this implementation uses little endian byte ordering and MD uses big endian,
-	// reverse all the bytes when copying the final state to the output hash.
-	for (i = 0; i < 4; ++i) {
-		hash[i]      = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
-		hash[i + 4]  = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
-		hash[i + 8]  = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
-		hash[i + 12] = (ctx->state[3] >> (24 - i * 8)) & 0x000000ff;
-		hash[i + 16] = (ctx->state[4] >> (24 - i * 8)) & 0x000000ff;
-	}
-}
-
-__kernel void gpusha1(__global unsigned long *pg_addrs, __global unsigned char* hashval, int text_num) {
+__kernel void gpuxxhash(__global unsigned long *pg_addrs, __global uint32_t* hashval, int text_num) {
 	int thx = get_global_id(0);
-        SHA1_CTX ctx;
-        int i;
         unsigned char text_dev[PAGE_SIZE];
-        unsigned char hashval_dev[SHA1_BLOCK_SIZE];
-      
+        uint32_t hashval_dev;
 
         if (thx < text_num  ) {
 
           __global unsigned char *pg_addr = pg_addrs[thx];
           for (i = 0; i < PAGE_SIZE; i++) {
-              text_dev[i] = pg_addr[i];  
+              text_dev[i] = pg_addr[i];
           }
-	  sha1_init(&ctx);
-	  sha1_update(&ctx, text_dev, PAGE_SIZE);
-          sha1_final(&ctx, hashval_dev);
-          for (i = 0; i < SHA1_BLOCK_SIZE; i++) {
-            *(hashval +thx * SHA1_BLOCK_SIZE + i) = hashval_dev[i];  
-            //debug *(hashval +thx * SHA1_BLOCK_SIZE + i) = (unsigned long)pg_addrs[thx] >> 8*i;  
-          }
-
-          
-
+	  hashval_dev = xxh32(text_dev, PAGE_SIZE, 0);
+          hashval[thx] = hashval_dev;
 	}
 }
 

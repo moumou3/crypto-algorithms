12b271f3a36334bc2917e2fcbf3ddc31476dc371
moumou <moumou3@users.noreply.github.com>
2019-06-06 17:35:01 +0900

    Modify for continuous check at ksm_run

--- ksmtest/counttime.c
+++ ksmtest/counttime.c
@@ -13,12 +13,14 @@ int main(int argc, char *argv[])
   char buffer[20];
   char buffer2[20];
   char buffer3[20];
+  char run_buffer[20];
   FILE *fp;
   FILE *fp2;
   FILE *fp3;
   FILE *fp4;
   FILE *fp5;
   FILE *fp6;
+  FILE *fp_result;
 
   fp = fopen("/sys/kernel/mm/ksm/pages_sharing", "r");
   fp2 = fopen("/sys/kernel/mm/ksm/pages_shared", "r");
@@ -30,25 +32,31 @@ int main(int argc, char *argv[])
   printf("pages_to_scan:%s", buffer);
   fread(buffer, 10, 1, fp5);
   printf("sleep_millisecs:%s", buffer);
-  fread(buffer, 10, 1, fp6);
-  printf("run:%s\n", buffer);
+  fread(run_buffer, 10, 1, fp6);
+  printf("run:%s\n", run_buffer);
 
   gettimeofday(&start,  NULL);  
   gettimeofday(&before,  NULL);  
   memset(buffer, 0x0, 20);
   memset(buffer2, 0x0, 20);
   memset(buffer3, 0x0, 20);
+  memset(run_buffer, 0x0, 20);
 
   while(1) {
     gettimeofday(&now, NULL);
     if (tv_sub(before, now) > 1.) {
+      fp_result = fopen("./result.csv", "a");
       fseek(fp, 0, SEEK_SET);
       fseek(fp2, 0, SEEK_SET);
       fseek(fp3, 0, SEEK_SET);
+      fseek(fp6, 0, SEEK_SET);
       fread(buffer, 10, 1, fp);
       fread(buffer2, 10, 1, fp2);
       fread(buffer3, 10, 1, fp3);
-      printf("time %fs:\n sharing:%s shared:%s full_scans:%s", tv_sub(start, now), buffer, buffer2, buffer3);
+      fread(run_buffer, 10, 1, fp6);
+      printf("time %fs:\n sharing:%s shared:%s full_scans:%s run:%s", tv_sub(start, now), buffer, buffer2, buffer3, run_buffer);
+      fputs(buffer, fp_result);
+      fclose(fp_result);
       gettimeofday(&before, NULL);
     }
     usleep(500000);
